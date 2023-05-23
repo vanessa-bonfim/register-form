348bf4b1562bb0d7207c6e5966c3ea0a1e79d13f
Vanessa Bonfim <vanesssabonfim@gmail.com>
2023-05-23 23:28:58 +0100

    Create comments

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import FormRegister from './components/FormRegister/FormRegister';
 import { Container, Typography } from '@mui/material';
 
-/* import '@fontsource/roboto'; */
+import '@fontsource/roboto'; // Importação de uma fonte externa 
+
 import './App.css';
+// Importação do arquivo de estilos CSS
+
 class App extends Component {
-  render(){
+  render() {
     return (
-      <Container component="article" maxWidth="sm" >
-        <Typography variant="h3" align='center' >Formulário de Cadastro</Typography> 
-        <FormRegister onSubmit={onSubmitForm} validationCPF={validateCPF}/>   
-      </Container >
+      <Container component="article" maxWidth="sm">
+        {/* Container principal do aplicativo com largura máxima definida */}
+        <Typography variant="h3" align='center'>
+          {/* Título do formulário */}
+          Formulário de Cadastro
+        </Typography>
+        <FormRegister onSubmit={onSubmitForm} validationCPF={validateCPF} />
+        {/* Componente do formulário de registro com funções de envio e validação */}
+      </Container>
     );
   }
 }
- function onSubmitForm(datas) {
-  console.log(datas);
- }
 
- function validateCPF(cpf) {
-  if(cpf.length !== 11){
-    return {valid: false, text:"O campo CPF deve ter 11 dígitos!"}
-  }else {
-    return {valid: true, text:""}
+function onSubmitForm(datas) {
+  console.log(datas);
+  // Função de callback para lidar com os dados enviados pelo formulário
+}
+
+function validateCPF(cpf) {
+  if (cpf.length !== 11) {
+    return { valid: false, text: "O campo CPF deve ter 11 dígitos!" };
+    // Validação do campo CPF para verificar se possui 11 dígitos
+  } else {
+    return { valid: true, text: "" };
   }
 }
+
 export default App;
+// Exportação do componente App como componente padrão do arquivo
--- src/components/FormRegister/FormRegister.jsx
+++ src/components/FormRegister/FormRegister.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Button, TextField, FormControlLabel, Switch } from "@mui/material";
 
 function FormRegister({ onSubmit, validationCPF }) {
-  /*
+/*
     Ao remover props e passa {onSubmit} como paramentro da função, é o mesmo que passar
     const onSubmit = props.onSubmit; dentro da função.
   */
@@ -13,83 +13,92 @@ function FormRegister({ onSubmit, validationCPF }) {
     Ao refatorar o de cima temos este código: const [name, setName] = useState();
 
     */
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
-  const [cpf, setCpf] = useState("");
-  const [sale, setSale] = useState(true);
-  const [news, setNews] = useState(false);
-  const [error, setError] = useState({cpf:{valid: true, text:""}});
+
+  // Definindo os estados iniciais
+  const [name, setName] = useState(""); // Estado para o nome
+  const [nickname, setNickname] = useState(""); // Estado para o sobrenome
+  const [cpf, setCpf] = useState(""); // Estado para o CPF
+  const [sale, setSale] = useState(true); // Estado para a opção de promoção
+  const [news, setNews] = useState(false); // Estado para a opção de novidades
+  const [error, setError] = useState({ cpf: { valid: true, text: "" } }); // Estado para o erro de validação do CPF
 
   return (
     <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        onSubmit({ name, nickname, cpf, sale, news });
+    // Manipulador de evento executado quando o formulário é enviado
+    onSubmit={(event) => {
+      event.preventDefault(); // Impede o comportamento padrão do envio do formulário
+      onSubmit({ name, nickname, cpf, sale, news }); // Chama a função onSubmit com os valores do formulário como argumento
+    }}
+  >
+    <TextField
+      value={name} // Valor do campo de texto obtido do estado
+      onChange={(event) => {
+        setName(event.target.value); // Atualiza o estado 'name' com o valor digitado no campo de texto
       }}
-    >
+      id="name" // ID do campo de texto
+      label="Nome" // Rótulo exibido acima do campo de texto
+      variant="outlined" // Estilo de borda do campo de texto
+      margin="dense" // Margem do campo de texto
+      fullWidth // Ocupa a largura total do contêiner
+    />
+      {/* Campo de texto para o sobrenome */}
       <TextField
-        value={name}
-        onChange={(event) => {
-          setName(event.target.value);
-        }}
-        id="name"
-        label="Nome"
-        variant="outlined"
-        margin="dense"
-        fullWidth
-      />
+  value={nickname} // Valor do campo de texto
+  onChange={(event) => { // Manipulador de evento para atualizar o estado "nickname"
+    setNickname(event.target.value);
+  }}
+  id="nickname" // ID do campo de texto
+  label="Sobrenome" // Rótulo exibido acima do campo de texto
+  variant="outlined" // Estilo de variante com borda delimitadora
+  margin="dense" // Margem densa
+  fullWidth // Ocupa toda a largura disponível
+/>
+      {/* Campo de texto para o CPF */}
       <TextField
-        value={nickname}
-        onChange={(event) => {
-          setNickname(event.target.value);
-        }}
-        id="nickname"
-        label="Sobrenome"
-        variant="outlined"
-        margin="dense"
-        fullWidth
-      />
-      <TextField
-        value={cpf}
-        onChange={(event) => {
-          setCpf(event.target.value);
-        }}
-        onBlur={(event) => {
-          const itsValid = validationCPF(cpf)
-          setError({cpf: itsValid})
-        }}
-        error={!error.cpf.valid}
-        helperText={error.cpf.text}
-        id="cpf"
-        label="CPF"
-        variant="outlined"
-        margin="normal"
-        fullWidth
-      />
+  value={cpf} // Valor do campo de texto
+  onChange={(event) => { // Manipulador de evento para atualizar o estado "cpf"
+    setCpf(event.target.value);
+  }}
+  onBlur={(event) => { // Manipulador de evento para executar a validação do CPF ao perder o foco
+    const itsValid = validationCPF(cpf); // Validação do CPF
+    setError({ cpf: itsValid }); // Atualiza o estado de erro "cpf"
+  }}
+  error={!error.cpf.valid} // Define se o campo de texto deve ser destacado como inválido
+  helperText={error.cpf.text} // Texto auxiliar exibido abaixo do campo de texto
+  id="cpf" // ID do campo de texto
+  label="CPF" // Rótulo exibido acima do campo de texto
+  variant="outlined" // Estilo de variante com borda delimitadora
+  margin="normal" // Margem normal
+  fullWidth // Ocupa toda a largura disponível
+/>
+      {/* Opção de promoção */}
       <FormControlLabel
-        control={
-          <Switch
-            checked={sale}
-            onChange={(event) => {
-              setSale(event.target.checked);
-            }}
-            name="sale"
-          />
-        }
-        label="Promoção"
+    control={
+      <Switch
+        checked={sale} // Valor do switch obtido do estado
+        onChange={(event) => {
+          setSale(event.target.checked); // Atualiza o estado 'sale' com o valor do switch
+        }}
+        name="sale" // Nome do switch
       />
+    }
+    label="Promoção" // Texto exibido ao lado do switch
+  />
+      {/* Opção de novidades */}
       <FormControlLabel
-        control={
-          <Switch
-            checked={news}
-            onChange={(event) => {
-              setNews(event.target.checked);
-            }}
-            name="news"
-          />
-        }
-        label="Novidades"
-      />
+  control={ // Componente de controle: Switch
+    <Switch
+      checked={news} // Estado do switch (marcado ou não)
+      onChange={(event) => { // Manipulador de evento para atualizar o estado "news"
+        setNews(event.target.checked);
+      }}
+      name="news" // Nome do switch
+    />
+  }
+  label="Novidades" // Rótulo exibido ao lado do switch
+/>
+
+      {/* Botão de cadastro */}
       <Button variant="contained" color="primary" type="submit">
         Cadastrar
       </Button>
